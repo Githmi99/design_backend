19634a118cd2434cf52ca63e90c48b5d91cdad0a
githmi99 <amarathuga@gmail.com>
2024-10-15 16:19:28 +0530

    url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
-import InsertProduct from './components/InsertProduct'
+import InsertProduct from './components/InsertProduct';
 import UpdateProduct from './components/UpdateProduct';
 import About from './components/About';
+import config from './config'; // Import config file
 
 import {
   BrowserRouter as Router,
@@ -12,10 +13,9 @@ import {
   Route
 } from 'react-router-dom';
 
-
-
-
 function App() {
+  console.log("Backend URL:", config.backendURL); // Use this URL for API calls
+
   return (
     <div className="App">
       <Navbar title="IMS" about="About"></Navbar>
@@ -24,15 +24,11 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/insertproduct" element={<InsertProduct />} />
-          <Route path="/updateproduct/:id" element={<UpdateProduct />} />
+          <Route path="/insertproduct" element={<InsertProduct backendURL={config.backendURL} />} />
+          <Route path="/updateproduct/:id" element={<UpdateProduct backendURL={config.backendURL} />} />
           <Route path="/about" element={<About />} />
-
         </Routes>
-
       </Router>
-
-
     </div>
   );
 }
---
+++ frontend/src/config.js
@@ -0,0 +1,7 @@
+// src/config.js
+const config = {
+    backendURL: "https://design-backend-nq2x.vercel.app"
+  };
+  
+  export default config;
+  
